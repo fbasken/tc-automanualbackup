4ccc13a9900c159957de9254a5c94fd090e6f9d7
Freddie <Freddie@ZETAWARE-XI>
2023-03-21 19:08:25 -0400

    Implemented backups

--- automatic_backup.py
+++ automatic_backup.py
@@ -5,14 +5,14 @@ import shutil
 from datetime import datetime
 from os.path import expanduser
 
-# C drive special folders
-# User special folders
+# TODO:
+# Change backup location to C:\ITS\_BACKUP
 
 
 REQUIRE_ADMIN = False
 
 # Backup root folder name
-backupFolderName = "_BACKUPS"
+backupFolderName = "_BACKUP"
 
 # Where to backup
 backupFolderPath = os.path.join(
@@ -24,8 +24,6 @@ folderPathsToCopy = [
         "AppData", "Local", "Microsoft", "Outlook", "Offline Address Books"),
     os.path.join(
         "AppData", "Local", "Microsoft", "Edge", "User Data", "Default"),
-    # os.path.join(
-    # "AppData", "Local", "Google", "Chrome", "User Data", "Default"),
     os.path.join(
         "AppData", "Local", "Mozilla", "Firefox", "Profiles"),
     os.path.join(
@@ -39,7 +37,7 @@ folderPathsToCopy = [
     os.path.join(
         "AppData", "Roaming", "Mozilla", "Firefox", "Profiles"),
     os.path.join(
-        "Contact"),
+        "Contacts"),
     os.path.join(
         "Desktop"),
     os.path.join(
@@ -60,7 +58,58 @@ folderPathsToCopy = [
         "Work Folders")
 ]
 
+standardItemsInUserFolder = [
+    ".cisco",
+    ".ms-ad",
+    "3D Objects",
+    "Contacts",
+    "Desktop",
+    "Documents",
+    "Downloads",
+    "Favorites",
+    "Links",
+    "Music",
+    "Pictures",
+    "Saved Games",
+    "Searches",
+    "Videos",
+    "Work Folders"
+]
 
+standardItemsInRootFolder = [
+    "$WinREAgent",
+    "APPDIR",
+    "Dell",
+    "Intel",
+    "ITS",
+    "MSOCache",
+    "PerfLogs",
+    "Program Files",
+    "Program Files (x86)",
+    "ProgramData",
+    "temp",
+    "Users",
+    "Windows",
+    "Config.Msi",
+    "Documents and Settings",
+    "Recovery",
+    "System Volume Information"
+]
+
+# Prevent OS sleep/hibernate in windows; code from:
+# https://github.com/h3llrais3r/Deluge-PreventSuspendPlus/blob/master/preventsuspendplus/core.py
+# API documentation:
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa373208(v=vs.85).aspx
+ES_CONTINUOUS = 0x80000000
+ES_SYSTEM_REQUIRED = 0x00000001
+
+def enableWindowsSleepPrevention(preventSleep):
+    if (preventSleep):
+        print("\n-- Preventing Windows from going to sleep")
+        ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
+    else:
+        print("\n-- Allowing Windows to go to sleep")
+        ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
 
 # https://stackoverflow.com/a/1026626
 # Returns true if application is running with elevated privileges
@@ -73,7 +122,12 @@ def isAdmin():
 
 # Main program method
 
+def isHidden(path):
+    return bool(ctypes.windll.kernel32.GetFileAttributesW(path) & 2)
 
+def displayHeader(headerText):
+    print("\n-- " + headerText)
+    
 def main():
 
     print("Backup path:\n" + backupFolderPath + "\n")
@@ -81,7 +135,11 @@ def main():
     while (os.path.exists(backupFolderPath)):
         print("Backup folder already exists. If you would like to perform a backup, please delete/move/rename the existing backup folder.")
         # print("Backup folder already exists. Would you like to make a new backup folder? ['y' to continue, anything else to exit] ")
-        input("Press Enter to retry\n")
+        input("Press Enter to retry.\n")
+
+    print()
+    backupRoot()
+    print()
 
     while True:
 
@@ -91,6 +149,7 @@ def main():
 
             if (username.lower().strip() == "exit"):
                 # End program
+                input("\n--\nBackup folder is on your Desktop.\nPress Enter to quit.")
                 sys.exit()
 
             userHomeFolder = getHomeFolderForUsername(username)
@@ -114,38 +173,169 @@ def main():
         backupUser(username)
         print()
 
+# Given a source path, and a destination folder, will copy the source file/folder (and subfolders)
+# and create a destination path if necessary
+def safeCopy(src_path, dest_path):
+    paddedOutput = src_path.ljust(40)
+
+    # Check if the file/folder to copy even exists
+    if (os.path.exists(src_path)):
+
+        # If the SOURCE path to copy is a FILE, copy it as a file
+        if os.path.isfile(src_path):
+            # Then, if the destination doesn't exist, make it
+            if(not os.path.exists(dest_path)):
+                os.makedirs(dest_path, exist_ok= True)
+
+            print("Copying file  | " + paddedOutput)
+            shutil.copy(src_path, dest_path)
+
+        # If the SOURCE path to copy is a FOLDER, copy it recursively
+        else:
+            # Then, if the destination DOES exist, this will FAIL
+            if(os.path.exists(dest_path)):
+                # This should not happen ever
+                print("Failed !!!    | " + paddedOutput + " | (Did not copy folder since destination already exists)")
+            else:
+                print("Copying dir   | " + paddedOutput)
+                shutil.copytree(src_path, dest_path)
+    else:
+        print("Skipping path | " + paddedOutput + " | (Does not exist)")
+
+def backupRoot():
+    print("[ BACKING UP ROOT FOLDERS (Non-user specific) ]")
+    
+    enableWindowsSleepPrevention(True)
+
+    # Back up C:\SAS
+    displayHeader("Backing up C:\\SAS\\")
+    src_path = "\\SAS"
+    dest_path = backupFolderPath
+
+    safeCopy(src_path, dest_path)
+
+    # Back up all non-standard folders on C:
+    displayHeader("Backing up non-standard folders on C:\\")
+    src_path = "\\"
+    dest_path = backupFolderPath
+
+    for filename in os.listdir("\\"):
+            fullFilePath = src_path + filename
+            paddedOutput = fullFilePath.ljust(40)
+            # Skip folders for these reasons:
+
+            # If it's not a folder (it's a file)
+            if os.path.isfile(fullFilePath):
+                print("Skipping path | " + paddedOutput + " | (Is a file, not a folder)")
+                continue
+            # If the folder is standard
+            if filename in standardItemsInRootFolder:
+                print("Skipping path | " + paddedOutput + " | (Standard folder)")
+                continue
+            # If the folder starts with . or $
+            if filename.startswith(".") or filename.startswith("$"):
+                print("Skipping path | " + paddedOutput + " | (Skipped due to prefix)")
+                continue
+            # If the folder is hidden
+            if(isHidden(fullFilePath)):
+                print("Skipping path | " + paddedOutput + " | (Hidden folder)")
+                continue
+            
+            safeCopy(fullFilePath, os.path.join(dest_path, filename))
+
+    enableWindowsSleepPrevention(False)
+
+    print("\n[DONE]")
 
 def backupUser(username):
+    print("[ BACKING UP USER: '" + username + "' ]")
+
+    enableWindowsSleepPrevention(True)
+
     userHomeFolder = getHomeFolderForUsername(username)
+    backupUsersFolderPath = os.path.join(backupFolderPath, "Users")
 
     # Back up each listed folder path
+    displayHeader("Backing up user folders and data")
     for relativePath in folderPathsToCopy:
         src_path = os.path.join(userHomeFolder, relativePath)
-        dest_path = os.path.join(backupFolderPath, username, relativePath)
+        dest_path = os.path.join(backupUsersFolderPath, username, relativePath)
 
-        if (os.path.exists(src_path)):
-            print("Copying path  | " + src_path)
-            # shutil.copytree(src_path, dest_path)
-        else:
-            print("Skipping path | " + src_path + " | (Folder not found)")
+        # safeCopy(src_path, dest_path)
     
     # Back up .PST files
     src_path = os.path.join(userHomeFolder, "AppData", "Local", "Microsoft", "Outlook")
-    src_path = os.path.join(userHomeFolder, "Desktop")
-    dest_path = os.path.join(backupFolderPath, username, "AppData", "Local", "Microsoft", "Outlook")
+    src_path = os.path.join(userHomeFolder, "Desktop") # DELETE THIS
+    dest_path = os.path.join(backupUsersFolderPath, username, "AppData", "Local", "Microsoft", "Outlook")
 
-
+    displayHeader("Backing up Microsoft Outlook .pst files")
     for filename in os.listdir(src_path):
         if filename.endswith(".pst"):
             fullFilePath = os.path.join(src_path, filename)
-            os.makedirs(dest_path, exist_ok= True)
             
-            print("Copying file  | " + fullFilePath)
-            shutil.copy(fullFilePath, dest_path)
+            safeCopy(fullFilePath, dest_path)
+
+    # Back up Chrome\User Data\Default, except for Service Worker folder
+    src_path = os.path.join(userHomeFolder, "AppData", "Local", "Google", "Chrome", "User Data", "Default")
+    dest_path = os.path.join(backupUsersFolderPath, username, "AppData", "Local", "Google", "Chrome", "User Data", "Default")
+
+    if os.path.exists(src_path):
+        displayHeader("Backing up Chrome User Data files (except Service Worker folder)")
+
+        for filename in os.listdir(src_path):
+            fullFilePath = os.path.join(src_path, filename)
+
+            # If we find a folder named 'Service Worker'
+            if filename == "Service Worker" and not os.path.isfile(fullFilePath):
+                # Skip the folder
+                continue
+            
+            safeCopy(fullFilePath, dest_path)
+
+    # Back up all non-standard folders in user folder
+    src_path = userHomeFolder
+    dest_path = os.path.join(backupUsersFolderPath, username)
+
+    if os.path.exists(src_path):
+        displayHeader("Backing up non-standard folders in '" + username + "' user folder")
+
+        for filename in os.listdir(src_path):
+            fullFilePath = os.path.join(src_path, filename)
+            paddedOutput = fullFilePath.ljust(40)
+
+            # Skip certain folders for these reasons:
+
+            # If it's not a folder (it's a file)
+            if os.path.isfile(fullFilePath):
+                print("Skipping path | " + paddedOutput + " | (Is a file, not a folder)")
+                continue
+            # If the folder is standard
+            if filename in standardItemsInUserFolder:
+                print("Skipping path | " + paddedOutput + " | (Standard folder)")
+                continue
+            # If the folder starts with . or $
+            if filename.startswith(".") or filename.startswith("$"):
+                print("Skipping path | " + paddedOutput + " | (Skipped due to prefix)")
+                continue
+            # If the folder is hidden
+            if(isHidden(fullFilePath)):
+                print("Skipping path | " + paddedOutput + " | (Hidden folder)")
+                continue
+
+            safeCopy(fullFilePath, os.path.join(dest_path, filename))
 
     # Hide AppData folder
-    ctypes.windll.kernel32.SetFileAttributesW(expanduser("~") + "\\Desktop\\_BACKUPS\\" + username + "\\AppData", 2)
+    appdataPath = os.path.join(backupUsersFolderPath, username, "AppData")
+    displayHeader("Marking AppData folder as hidden " + appdataPath)
+    if os.path.exists(appdataPath):
+        ctypes.windll.kernel32.SetFileAttributesW(appdataPath, 2)
+        print("AppData folder for '" + username + "' successfully hidden.")
+    else:
+        print("Error! Could not hide AppData folder-- AppData folder for '" + username + "' does not exist.")
+    
+    enableWindowsSleepPrevention(False)
 
+    print("\n[DONE]")
 
 # Returns the path to the user's home folder
 def getHomeFolderForUsername(username):
